12a3b62a546da2c196cc32a37d6aac9cfc82c185
Andriy Arterchuk <andriyarterchuk@MacBook-Pro-Andriy.local>
2019-04-11 01:27:38 +0300

    add docker files

--- services/Hotel/index.ts
+++ services/Hotel/index.ts
@@ -1,7 +1,17 @@
-import router from "BaseService/rest/router";
+// import router from "BaseService/rest/router";
 
-router.get("/", async (ctx, next) => {
-  ctx.body = "Hello World";
+// router.get("/", async (ctx, next) => {
+//   ctx.body = "Hello World";
+// });
+
+// export default router;
+
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async function(ctx) {
+  ctx.body = JSON.stringify([{name: 'test'}]);
+  ctx.status = 200;
 });
 
-export default router;
+app.listen(3000, () => console.log('started'));
